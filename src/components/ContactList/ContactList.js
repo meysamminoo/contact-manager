8e8b3f505cfb4f15592a0752ff811660df5e2c4f
meysam <meisam.design.ph@gmail.com>
2022-03-24 22:25:27 +0430

    add search onbase name and email

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -6,6 +6,7 @@ import { deleteOneContact, getContacts } from "../../services/ContactsServices";
 
 const ContactList = () => {
   const [contacts, setContacts] = useState([]);
+  const [allContacts, setAllContacts] = useState(null);
   const [searchTerm, setSerchTerm] = useState("");
 
   const removeHandler = async (id) => {
@@ -20,6 +21,7 @@ const ContactList = () => {
     const fetchContacts = async () => {
       const { data } = await getContacts();
       setContacts(data);
+      setAllContacts(data);
     };
 
     try {
@@ -29,6 +31,19 @@ const ContactList = () => {
 
   const seachHandler = (e) => {
     setSerchTerm(e.target.value);
+    const search = e.target.value;
+
+    if (search !== "") {
+      const filtredContact = allContacts.filter((c) => {
+        return Object.values(c)
+          .join(" ")
+          .toLowerCase()
+          .includes(search.toLowerCase());
+      });
+      setContacts(filtredContact);
+    } else {
+      setContacts(allContacts);
+    }
   };
 
   return (
@@ -38,9 +53,15 @@ const ContactList = () => {
         <Link to="/add">
           <button>Add Contact</button>
         </Link>
-        <div>
-          <input type="text" value={searchTerm} onChange={seachHandler} />
-        </div>
+      </div>
+      <div className={styles.searchBox}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={seachHandler}
+          className={styles.search}
+          placeholder="search contact"
+        />
       </div>
       {contacts.map((contact) => (
         <Contact key={contact.id} contact={contact} onRemove={removeHandler} />
